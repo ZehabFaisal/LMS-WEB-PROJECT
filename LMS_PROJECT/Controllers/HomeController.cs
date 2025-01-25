9aa05a2395437627a4f3268e9e009505a81d8d9d
HP <HP@DESKTOP-A0J5KSE>
2025-01-25 15:43:13 +0500

    Add project files.

---
+++ LMS_PROJECT/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System.Web.Mvc;
+
+namespace LMS_PROJECT.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+        public ActionResult Courses()
+        {
+            return View();
+        }
+
+        public ActionResult Reviews()
+        {
+            return View();
+        }
+        public ActionResult Blog()
+        {
+            return View();
+        }
+        public ActionResult Teacher()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+    }
+}
